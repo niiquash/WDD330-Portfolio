99b3ef99b64d9916982671a94a76b8a5e121d020
niiquash <qua16006@byui.edu>
2022-04-22 13:13:23 -0600

    added js file.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let orderedList = document.querySelector("ol");
+
+const links = [
+    {label: "Week1 notes",
+     url: "week1/index.html"}
+]
+
+links.forEach(() => {
+    const listItem = document.createElement("li");
+    listItem.appendChild(item);
+})
