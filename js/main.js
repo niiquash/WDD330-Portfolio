68ce2879969e98d8e55d0dbc9e3234599cc129b0
niiquash <qua16006@byui.edu>
2022-04-22 14:22:47 -0600

    dynamically added array items for table of contents.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 let orderedList = document.querySelector("ol");
 
 const links = [
-    {label: "Week1 notes",
+    {label: "Week-1 Notes",
      url: "week1/index.html"}
 ]
 
 links.forEach((item) => {
+    console.log(item["label"]);
     const listItem = document.createElement("li");
-    listItem.appendChild(item);
+    const itemLink = document.createElement("a");
+
+    itemLink.setAttribute('href', item['url']);
+    itemLink.innerHTML = item['label']
+
+    listItem.appendChild(itemLink);
+
+    orderedList.appendChild(listItem);
 })
