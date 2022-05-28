02f9ca693d6feaeebdf4c09ef88d408299ce4ad2
Ammon Quarshie <niiquash@churchofjesuschrist.org>
2022-05-28 01:44:00 -0600

    added link for week 6

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@ const links = [
         label: "Week-5",
         url: "week5/index.html"
     },
+    {
+        label: "Week-6",
+        url: "week6/index.html"
+    }
 ]
 
 links.forEach((item) => {
